5d0b8420cf906897859c84a7545867776590d500
Tiyasha Mallick <20051855@kiit.ac.in>
2023-12-04 23:55:17 +0530

    .

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-help',
@@ -6,13 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent {
-  firstName: string = '';
-  lastName: string = '';
-  email: string = '';
-  phone: string = '';
-  message: string = '';
+  form: FormGroup = this.fb.group({
+    name:'',
+    email:'',
+    message:''
+  });
 
-  Send() {
+  constructor(private fb: FormBuilder){}
+
+  async Send() {
+    emailjs.init('sNSFgqes1kXmUrcNo');
+    let response = await emailjs.send("service_jz497qj","template_vhk1nmd",{
+      name:this.form.value.name,
+      email:this.form.value.email,
+      message:this.form.value.message
+    });
     // Display alert message
     window.alert('Message sent!');
     window.location.reload();
